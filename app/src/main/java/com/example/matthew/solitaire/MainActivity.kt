a72736733975d65756da9426eafd36c86db1e248
matedge <mat.edge@gmail.com>
2018-05-18 17:15:52 +1000

    started android for solitaire_kotlin

---
+++ app/src/main/java/com/example/matthew/solitaire/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.matthew.solitaire
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        verticalLayout {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/matthew/solitaire/solataire_kotlin_logic/GameView.kt
@@ -0,0 +1,5 @@
+package com.example.matthew.solitaire
+
+interface GameView {
+    fun update(model: GameModel = GameModel )
+}
